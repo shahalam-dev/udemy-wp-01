19af00a7f59ebcde41f3111980329e13941a0ac5
shahalam sharif <shahalam.shareef@gmail.com>
2021-03-01 17:07:58 +0600

    sec-06_22

--- index.php
+++ index.php
@@ -26,7 +26,9 @@
                   <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
                 </div>
               </div>
-        <?php  } ?>
+        <?php  } 
+          echo paginate_links();
+        ?>
       </div>
     
 <?php
